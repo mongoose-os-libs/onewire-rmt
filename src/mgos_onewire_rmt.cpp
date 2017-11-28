acdb58d961a4adeace9c97a17ebac79c37e3b5a9
Liviu <nliviu@gmail.com>
2017-11-28 21:02:16 +0200

    Add files via upload

---
+++ src/mgos_onewire_rmt.cpp
@@ -0,0 +1,79 @@
+#include "OnewireRmt.h"
+#include "mgos_onewire_rmt.h"
+
+OnewireRmt* mgos_onewire_rmt_create(int pin)
+{
+    return new OnewireRmt(pin);
+}
+
+void mgos_onewire_rmt_close(OnewireRmt *ow)
+{
+    delete ow;
+}
+
+bool mgos_onewire_rmt_reset(OnewireRmt *ow)
+{
+    return ow->reset();
+}
+
+uint8_t mgos_onewire_rmt_crc8(const uint8_t *rom, int len)
+{
+    return OnewireRmt::crc8(rom, len);
+}
+
+void mgos_onewire_rmt_target_setup(OnewireRmt *ow, const uint8_t family_code)
+{
+    ow->target_search(family_code);
+}
+
+bool mgos_onewire_rmt_next(OnewireRmt *ow, uint8_t *rom, int mode)
+{
+    return ow->search(rom, mode);
+}
+
+void mgos_onewire_rmt_select(OnewireRmt *ow, const uint8_t *rom)
+{
+    ow->select(rom);
+}
+
+void mgos_onewire_rmt_skip(OnewireRmt *ow)
+{
+    ow->skip();
+}
+
+void mgos_onewire_rmt_search_clean(OnewireRmt *ow)
+{
+    ow->reset_search();
+}
+
+bool mgos_onewire_rmt_read_bit(OnewireRmt *ow)
+{
+    return ow->read_bit();
+}
+
+uint8_t mgos_onewire_rmt_read(OnewireRmt *ow)
+{
+    return ow->read();
+}
+
+void mgos_onewire_rmt_read_bytes(OnewireRmt *ow, uint8_t *buf, int len)
+{
+    ow->read_bytes(buf, len);
+}
+
+void mgos_onewire_rmt_write_bit(OnewireRmt *ow, int bit)
+{
+    ow->write_bit(bit);
+}
+
+void mgos_onewire_rmt_write(OnewireRmt *ow, const uint8_t data)
+{
+    ow->write(data);
+}
+
+void mgos_onewire_rmt_write_bytes(OnewireRmt *ow, const uint8_t *buf, int len)
+{
+    ow->write_bytes(buf, len);
+}
+
+
